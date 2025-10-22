a770a2f5a01a9d64bc06c6542d7c7f1bdb95208f
VAN DAI DO <s223540177@h100-m-05.ai.deakin.edu.au>
2025-10-22 18:24:20 +1100

    convert data

--- run_convert_dataset.py
+++ run_convert_dataset.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import json
 
-in_path  = "/scratch/s223540177/Time-Series-Library/data/all_datasets/ETT-small/ETTh2.csv"
-out_path = "/scratch/s223540177/Time-Series-Library/data/all_datasets/ETT-small/ETTh2.jsonl"
+in_path  = "/scratch/s223540177/Time-Series-Library/data/all_datasets/illness/national_illness.csv"
+out_path = "/scratch/s223540177/Time-Series-Library/data/all_datasets/illness/national_illness.jsonl"
 
 # Read CSV
 df = pd.read_csv(in_path)
@@ -11,14 +11,14 @@ df = pd.read_csv(in_path)
 if "date" in df.columns:
     df = df.drop(columns=["date"])
 
-
 # (Optional) handle NaNs — here we drop rows that have any NaN
 df = df.dropna(axis=0, how="any")
 
-# Write JSONL as one sequence per row
+# Write JSONL as one sequence per COLUMN (first column sequence, then second, etc.)
 with open(out_path, "w") as f:
-    for _, row in df.iterrows():
-        rec = {"sequence": row.tolist()}
+    for col in df.columns:
+        seq = df[col].tolist()
+        rec = {"sequence": seq}
         f.write(json.dumps(rec) + "\n")
 
-print(f"Wrote {len(df)} lines to {out_path}")
+print(f"Wrote {df.shape[1]} lines to {out_path}")
